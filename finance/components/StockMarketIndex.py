747a5564acd50749aea32efea9cf013b1200aaed
Stephan Abend <st.abend@googlemail.com>
2020-03-26 00:08:46 +0100

    Initial commit of class StockMarketIndex

---
+++ finance/components/StockMarketIndex.py
@@ -0,0 +1,9 @@
+"""
+Class definition for stock market index.
+"""
+from src.data.YahooDataLoader import YahooDataLoader
+from src.data.IngDataScraper import IngDataScraper
+
+
+class StockMarketIndex:
+    pass
